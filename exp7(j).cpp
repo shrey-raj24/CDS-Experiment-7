421f2b494cdbb201c65b82c7dc83743255d1badd
Shrey raj <shreyraj37@gmail.com>
2024-08-21 01:57:54 +0530

    Add files via upload

---
+++ exp7(j).cpp
@@ -0,0 +1,28 @@
+// NAME - SHREY RAJ
+// PRM - 23070123123
+// EXPERIMENT - 7(J)  
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+string a;
+cout<<"Enter a string: ";
+cin>>a;
+int n=a.length(), i, flag=0;
+for(i=0;i<a.length();i++){
+if(a[i]==a[n-1]){
+flag=1;
+}
+n--;
+}
+if(flag==1){
+cout<<a<<" is palindrome";
+}
+else{
+cout<<a<<" is not palindrome";
+}
+}
+
+// Enter a string: Malayalam 
+// Malayalam is palindrome
