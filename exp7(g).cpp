1486d4e91e3a1c04bf86d8fd5a85dbb894a97068
Shrey raj <shreyraj37@gmail.com>
2024-08-21 01:56:27 +0530

    Add files via upload

---
+++ exp7(g).cpp
@@ -0,0 +1,19 @@
+// NAME - SHREY RAJ
+// PRM - 23070123123
+// EXPERIMENT - 7(G) 
+
+#include<iostream> 
+#include<string>
+using namespace std; 
+
+int main() 
+{
+    string a;
+    cout<<"Enter any word: "; 
+    cin>>a;
+    cout<<"Entered string is "<<a<<"."<<"\n";
+    return 0;
+}
+
+// Enter any word: Environment
+// Entered string is Environment.
