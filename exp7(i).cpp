9b5fd88030623aaab71f289b2192e492ac116177
Shrey raj <shreyraj37@gmail.com>
2024-08-21 01:57:01 +0530

    Add files via upload

---
+++ exp7(i).cpp
@@ -0,0 +1,20 @@
+// NAME - SHREY RAJ 
+// PRM - 23070123123 
+// EXPERIMENT - 7(I) 
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+string a;
+cout<<"Enter string: ";
+cin>>a;
+int i;
+for(i=a.length()-1;i>=0;i--) {
+cout<<a[i];
+}
+return 0;
+}
+
+// Enter string: Shloka 
+// akolhS 
