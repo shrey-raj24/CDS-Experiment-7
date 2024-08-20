55238137821ae2836d7edf4a9f66f81c3f28d828
Shrey raj <shreyraj37@gmail.com>
2024-08-21 01:56:43 +0530

    Add files via upload

---
+++ exp7(h).cpp
@@ -0,0 +1,19 @@
+// NAME - SHREY RAJ
+// PRM - 23070123123
+// EXPERIMENT - 7(H) 
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+string a, b;
+cout<<"Enter strings: ";
+cin>>a>>b;
+cout<<"CONCATENATION: "<<a+b;
+
+return 0;
+} 
+
+// Enter strings: Slow
+// Fast
+// CONCATENATION: SlowFast
